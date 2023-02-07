de5a9c8432f1d884ca83730286608a7c4631ff8d
Fckroun <69816382+Fckroun@users.noreply.github.com>
2023-02-07 10:44:40 +0100

    Fubifs.py plugin
    Extract UBI + squashfs

---
+++ Fubifs.py
@@ -0,0 +1,20 @@
+import binwalk.core.plugin
+import subprocess,os
+
+class FUBIFSExtractor(binwalk.core.plugin.Plugin):
+    MODULES = ['Signature']
+    def scan(self, result):
+        if result.valid and result.description.startswith("UBI erase"):
+            offset = result.offset
+            filename = result.file.name
+            out_filename = filename + ".ubirootfs"
+            print(filename)
+            cmd1 = f"ubireader_extract_images {filename} -o {out_filename}"
+            subprocess.call(cmd1, shell=True)
+            directory = out_filename
+            for subdir, dirs, files in os.walk(directory):
+                for i in files:
+                    if 'rootfs' in i :
+                        cmd2 = f"binwalk -Me {os.path.join(subdir, i)}"
+                        subprocess.call(cmd2, shell=True)
+                        exit(0)

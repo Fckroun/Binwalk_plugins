5df42e306f73fb5bac15a4f6f1f5656f7167da5b
Fckroun <69816382+Fckroun@users.noreply.github.com>
2023-02-17 14:22:26 +0100

    Working squashfs plugin

--- Fsqshv4.py
+++ Fsqshv4.py
@@ -1,14 +1,21 @@
+import os
+import subprocess
 import binwalk.core.plugin
-import subprocess,os
 
-class Fsqshv4(binwalk.core.plugin.Plugin):
+class Fsqshv4be(binwalk.core.plugin.Plugin):
     MODULES = ['Signature']
-    def scan(self, result):
-        if result.valid and result.description.startswith("Squashfs filesystem, big endian, version 4.0"):
-            offset = result.offset
-            filename = result.file.name
-            out_filename = filename + ".squashfs-root"
-            print("***** SQUASHFS V4 BE ******")
-            cmd1 = f"/bin/sasquatch-v4be -be -no-exit-code -f -d\"{out_filename}\" \"{filename}\""
-            subprocess.call(cmd1, shell=True)
-            exit(0)
+    def init(self):
+        if self.module.extractor.enabled:
+            self.module.extractor.add_rule(txtrule=None,
+                                           regex=".*quashfs filesystem, big endian, version 4.0.*",
+                                           extension="squashfs",
+                                           cmd=self.extractor)
+
+    def extractor(self, fname):
+        fname = os.path.abspath(fname)
+        outfile = os.path.splitext(fname)[0]
+        print("EXTRACTOR!!")
+        cmd1 = f"/bin/sasquatch-v4be -f -d \"{outfile}\"  \"{fname}\""
+        print(cmd1)
+        subprocess.call(cmd1, shell=True)
+        return True
